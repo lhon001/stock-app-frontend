4e7998e7242c89e59fd33a8bac15ff4d14101282
github username <github email address>
2018-08-29 10:43:14 -0400

    retry URL link

--- src/adapter.js
+++ src/adapter.js
@@ -1,5 +1,5 @@
 // const baseURL = `http://localhost:3000/api/v1`
-const baseURL = `https://stock-app-backend.herokuapp.com/`
+const baseURL = `https://stock-app-backend.herokuapp.com/api/v1`
 
 export function getStockInfo(symbol) {
   const url = `${baseURL}/search/${symbol}`
